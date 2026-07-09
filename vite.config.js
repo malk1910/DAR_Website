5da016d2bc319763a15c226fe99d52d34595b35e
malk1910 <engmalkayman2005@gmail.com>
2026-07-09 19:32:25 +0300

    editing navbar

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // base: "/DAR_Website/",
+  build: {
+    cssMinify: 'esbuild' // Restores standard CSS behavior
+  }
 })
